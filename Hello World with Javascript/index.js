344cc7a56fa946b42b237c11625860b735001a58
정표용 <sur2@naver.com>
2020-06-13 23:58:14 +0900

    javascript array

--- Hello World with Javascript/index.js
+++ Hello World with Javascript/index.js
@@ -1,1 +1,11 @@
 console.log('I'+"'"+'m Working. Im JS. Im Beautiful. Im worth it.');
+/* 
+const a = 123;
+a = 4; 
+ TypeError: Assignment to constant variable.
+ */
+
+ let a = 123;
+ // Array
+ const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+ console.log(daysOfWeek[0]);
